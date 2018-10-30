52f87f5c26d8ec5d5a68458b6a187b8741803632
apllefresh <arrogant93@mail.ru>
2018-10-30 20:13:10 +0300

    filter1

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ class MyTable extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { data: makeData(), columns : getColumns(), options : getOptions(), inputList : getInputs() };
-     this.hideInput = this.hideInput.bind(this);
+    this.state = { data: makeData(), columns : getColumns(), options : getOptions(), inputList : getInputs(), filterList : getFilters() };
+     //this.hideInput = this.hideInput.bind(this,this.id);
+      this.state.filter = '';
   }
 
   
@@ -28,15 +29,20 @@ class MyTable extends React.Component {
           newOptions.push(newOption);
         }
       }
-      var index = this.state.inputList.indexOf(event.value);
-      
     this.setState({options:  newOptions  });
      this.setState({input: this.reLoad() });  
   };
 
-  hideInput = () => {
-       var newOptions = [];
+  hideInput = function(e) {
+     // console.log(e );
+  
+        var newOptions = [];
       for (var i = this.state.inputList.length - 1; i >= 0; i--) {
+          if (this.state.inputList[i].value === e) 
+              {
+                this.state.inputList[i].view = 0;
+                this.state.filterList[i].value = '';
+              }
           if (this.state.inputList[i].view === 0)
           {
           var newOption = {
@@ -50,36 +56,68 @@ class MyTable extends React.Component {
       this.setState({options:  newOptions  });
 
        this.setState({input: this.reLoad() });  
+       
     }
+
+     changeInput = function (e)  
+     {
+        var _filter='';
+        for (var i = this.state.filterList.length - 1; i >= 0; i--) 
+        {
+          if (this.state.filterList[i].key === e.id) this.state.filterList[i].value = e.value;
+          if (this.state.filterList[i].value.trim().length > 0)  
+            {
+              if (_filter.length >= 1) _filter = _filter + ' and ';
+                _filter = _filter + this.state.filterList[i].key + ' like "%' + this.state.filterList[i].value+'%" ';
+              
+            }
+        }
+        console.log(_filter);
+        this.setState({filter: _filter});
+     }
 
   reLoad = function ()  
   {
-      return (
-        <div onClick={()=> this.hideInput() }>
-      {this.state.inputList.map(function(name, index){
-                     if (name.view === 1)
-                    return ( 
-                        <div key={ index }>
-                          <input type="text" value={name.value} key={ index } />
-                          <button onClick={()=> name.view = 0 } id={name.value} key={ index } >X</button>
-                        </div>)
-                  })}
-          </div>
-          )
+      var t =[];
+      for (var i = this.state.inputList.length - 1; i >= 0; i--) {
+        if (this.state.inputList[i].view === 1)
+          {
+            this.setState({idd : i.toString()})    ;
+            var ttt = 
+                        <div>
+                          <input type="text" id={this.state.inputList[i].value} placeholder={this.state.inputList[i].value} onChange={(e)=>this.changeInput(e.target)} />
+                          <button onClick={(i)=>{this.hideInput(i.target.id)}} id={this.state.inputList[i].value}>X</button>
+                        </div>;
+            t.push(ttt);
+          } 
+      }
+      //console.log(t);
+
+        return (
+            <div>
+              {t.map(function(tt, index)
+                {
+                     return ( 
+                            <div key={ index }>
+                              {tt}
+                            </div>)
+              })}
+            </div>);
+              
   }
   
  
-
   render() {
     return (
+      
       <div>
+       <div>
        {this.state.input}
+        </div>
       <Dropdown options={this.state.options} onChange={this.handleChange} placeholder="Select an option" />
-        <ReactTable
-          data={this.state.data}
-           columns={this.state.columns}
-        />
+        <ReactTable    data={this.state.data}    columns={this.state.columns}   />
       </div>
+      
     );
   }
 }
@@ -127,20 +165,24 @@ function getColumns() {
       {
             Header: "id",
             accessor: d => d.id,
-            id: "id"
+            id: "id",
+            show: false
       },
       {
             Header: "First Name",
-            accessor: "firstName"
+            accessor: "firstName",
+            type : "text"
       },
       {
             Header: "Last Name",
-            accessor: "lastName"
-            
+            accessor: "lastName",
+            type : "select",
+            options : [ {label : "1", value : "2"}]
       },
       {
         Header: "Info",
-        accessor: "age"
+        accessor: "age",
+        type : "checkbox"
       }
     ];
 }
@@ -174,7 +216,29 @@ function getInputs() {
           var transformed = {
               label: col[i].Header ,
               value: col[i].accessor,
-              view : 0
+              view : 0,
+              type : col[i].type,
+              options : col[i].options
+          };
+        newInputs.push(transformed);
+        }
+  }
+
+  return newInputs;
+
+}
+
+function getFilters() {
+
+  var newInputs = [];
+  var col = getColumns();
+  for (var i = 0; i < col.length; i++) {
+      if (col[i].Header !== "id") 
+      {
+          var transformed = {
+              key: col[i].accessor,
+              value : ''
+              
           };
         newInputs.push(transformed);
         }
